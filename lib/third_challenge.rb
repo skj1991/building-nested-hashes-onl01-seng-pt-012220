ec3327e4252457a1af7de7dfeaf05a2a12b2706e
Stacy Johns <stacykjohns@icloud.com>
2020-01-31 23:42:05 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,19 +1,18 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
-        :name => "",
-        :age => ""
-        },
-      :matriarch => {
-        :name => "",
-        :age => ""
-        },
-      :hero => {
-        :name => "",
-        :age => "",
-        :status => ""
-        },
+     :patriarch => {
+     :name => "Lord Montague",
+     :age => "53"
+ },
+   :matriarch => {
+     :name => "Lady Montague",
+     :age => "54"
+ },
+   :hero => {
+     :name => "Romeo",
+     :age => "15"
+ },
       :hero_friends => []
    },
    :capulet => {
